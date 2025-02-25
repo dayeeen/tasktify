7c74fb7339aed07dd8e92d3fc097e5703cc57b95
dayeeen <nubicodes@gmail.com>
2025-02-25 22:47:31 +0700

    Refactor users migration and add tasks migration (before that I used to make db without using migration)

---
+++ database/migrations/2025_02_25_154227_create_tasks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id'); // Relasi dengan tabel users
+            $table->string('title');
+            $table->text('description');
+            $table->timestamp('finished_at')->nullable();
+            $table->timestamp('deadline')->nullable();
+            $table->enum('priority', ['high', 'medium', 'low']); // Kolom priority dengan enum
+            $table->enum('status', ['not_started', 'on_progress', 'completed', 'expired'])->default('not_started'); // Kolom status dengan enum
+            $table->timestamps(); // created_at, updated_at
+
+            // Relasi dengan tabel users
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
